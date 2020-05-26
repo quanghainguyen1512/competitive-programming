a3b903e65859d08fb0a9aafc2044e5433a2bcc0b
Hai Nguyen <quanghai.ng@outlook.com.vn>
2020-05-26 18:01:06 +0700

    remove files

--- SPOIJ/spoij-MICEMAZE.py
+++
@@ -1,8 +0,0 @@
-n = int(input())
-e = int(input())
-t = int(input())
-m = int(input())
-graph = [[0 for i in range(n)] for i in range(n)]
-while m != 0:
-    a, b, w = map(int, input().split())
-    m -= 1
