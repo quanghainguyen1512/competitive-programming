c64550b8620b2de0f95525ace52135aba6acebbf
Hai Nguyen <quanghai.ng@outlook.com.vn>
2020-08-24 16:50:22 +0700

    leetcode

---
+++ leetcode/two-sum.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/two-sum/
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        store = {}
+        for i, e in enumerate(nums):
+            if e in store:
+                return [store[e], i]
+            tmp = target - e
+            store[tmp] = i
